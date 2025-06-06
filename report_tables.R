712dabd684477f3903670996394eaeb288e0262d
Arni Magnusson <thisisarni@gmail.com>
2025-06-06 16:44:26 +1100

    Compare length conversions

--- report_tables.R
+++ report_tables.R
@@ -2,7 +2,7 @@
 
 # Before: otoliths.csv (data), conversions.RData (output)
 #         par_female.csv, par_male.csv, par_pooled.csv (output)
-# After:  samples.csv (report)
+# After:  conversions.csv, samples.csv (report)
 
 library(TAF)
 library(areaplot)
@@ -25,5 +25,15 @@ samples <- data.frame(sex=c("female", "male"),
                       row.names=NULL)
 samples <- rnd(samples, -1)
 
-# Write table
+# Compare conversions
+eqs <- c("L = 1.1111 * E",
+         "L = 6.898 + 1.0753 * E",
+         "L = 7.7911 + 1.0647 * E",
+         "L = 10.323 + 1.0559 * E")
+par <- c(p1$est[1:3], p2$est[1:3], p3$est[1:3], p4$est[1:3])
+par <- matrix(par, ncol=3, byrow=TRUE, dimnames=list(1:4, c("L1", "L2", "k")))
+conversions <- data.frame(eq=eqs, par)
+
+# Write tables
+write.taf(conversions, dir="report")
 write.taf(samples, dir="report")
